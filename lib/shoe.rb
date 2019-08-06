863689bcd10e25c610f4beaee895b90aa8a50dd0
Polly Reynolds <polly.c.reynolds@gmail.com>
2019-08-06 13:35:18 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,8 @@
-# Make your shoe class here!
+class Shoe
+  def initialize(brand)
+    @brand = brand
+    @color = color
+    @size = size
+    @material = material
+    @condition = condition
+end 
